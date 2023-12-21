c3c701522d59b2a9d8ac86aabe0c28b01e5ba218
hiromasa morita <morimasahirota@gmail.com>
2023-12-21 23:35:37 +0900

    update

--- scripts/display.py
+++ scripts/display.py
@@ -6,18 +6,18 @@ from matplotlib import pyplot as plt
 
 
 
-img1 = cv2.imread('/home/morita/ros2_ws/output/rectangle07.jpg')
-img2 = cv2.imread('/home/morita/ros2_ws/output/rectangle09.jpg')
+img1 = cv2.imread('/home/morita/ros2_ws/Memo/tex/img/img1.png')
+img2 = cv2.imread('/home/morita/ros2_ws/Memo/tex/img/img3.png')
 
 
 img1 = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
 img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
 
 
-plt.subplot(121),plt.imshow(img2)
-plt.title('Profile of blonde woman'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img1)
-plt.title('Profile of young woman'), plt.xticks([]), plt.yticks([])
+plt.subplot(121),plt.imshow(img1)
+plt.title('link coordinate'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img2)
+plt.title('ADA031'), plt.xticks([]), plt.yticks([])
 
 plt.show()
 
