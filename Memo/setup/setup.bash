bd1aaf324d37fe8cdc87264447c74f5d2656f66e
hiromasa morita <morimasahirota@gmail.com>
2023-12-25 10:48:24 +0900

    uupdate

--- Memo/setup/setup.bash
+++ Memo/setup/setup.bash
@@ -8,5 +8,12 @@ cd ~/ros2_setup_scripts.git
 ./setup.bash
 cd
 
+# 必要なパッケージをインストール
 sudo apt-get install git vim curl unzip imagemagick texlive-lang-japanese  texlive-latex-extra xdvik-ja evince ibus-mozc shotwell
+
+# pip3 をインストール
+sudo apt install python3-pip
+pip3 install Jupyter jupyterlab pandas numpt Scipy
+
+export PATH="$HOME/.local/bin:$PATH"  # PATHの追加
 
